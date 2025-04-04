053643657055678818824857f6a22d3d1adaf160
Mahmudtareq <tareqlpu5@gmail.com>
2025-04-04 23:24:49 +0600

    initial project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+  return <div className="">hello world Homepage</div>;
+};
 
-export default Homepage
+export default Homepage;
